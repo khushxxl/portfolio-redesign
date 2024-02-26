3a4a2530696213f0d942b14ff6842efc9d69b0a8
Khushaal Choithramani <khushaal.choithramani@gmail.com>
2024-02-26 19:23:14 +0000

    new chnages update

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -10,12 +10,12 @@ function Skills() {
       <h1 className="text-gray-500 text-sm font-semibold  tracking-wide">
         Skills that I excel and have a lot of experience inn 💪
       </h1>
-      <div className="grid grid-cols-2 md:grid-cols-3 place-items-center text-justify">
+      <div className="grid  grid-cols-2 md:grid-cols-3 place-items-center text-justify">
         {skills?.map((data, i) => {
           return (
             <div
               key={i}
-              className="bg-slate-800 min-w-[170px] text-center  p-[3px] mt-5 rounded-md"
+              className="bg-slate-800 transform hover:scale-110 transition-all duration-150 cursor-pointer min-w-[170px] text-center  p-[3px] mt-5 rounded-md"
             >
               <h1>{data}</h1>
             </div>
